d57992dde936ba3566e1358b2b0b7d63db12a1e0
TamilkodiVinayagam <tamilkodimillioniare@gmail.com>
2023-10-25 17:22:58 +0530

    changes in css file

--- colorflipper/colorflipper.js
+++ colorflipper/colorflipper.js
@@ -1,22 +1,18 @@
-let hex=document.getElementById("hex")
+let colorText=document.getElementById("hex")
 
 let wrap=document.getElementById("wrap")
 let btn=document.querySelector("#btn")
+const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 function hexColor(){
-   const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+   
     let hexcolor=Math.floor(Math.random()*hex.length)
     return hex[hexcolor]
 }
-function colorCode(){
-    
-    let str="#"
-for(let i=0;i<6;i++){
-str=str+hexColor()
-}
-return str
-}
-
 btn.addEventListener("click",function(){
-    wrap.style.backgroundColor=colorCode()
-    hex.innerHTML=colorCode()
+    let colorStr="#"
+for(let i=0;i<6;i++){
+colorStr=colorStr+hexColor()
+}
+    wrap.style.backgroundColor=colorStr
+    colorText.innerHTML=colorStr
 })
